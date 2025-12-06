f09ea8af77f9cdd7db9a631f0d63268dbff5e2b0
Sarah Cristina <cristinasarah@discente.ufg.br>
2025-12-06 13:44:16 -0300

    adicionei as funções de editar e mostrar terreno

--- terrenos.c
+++ terrenos.c
@@ -73,11 +73,82 @@ void deletarTerreno(Terrenos ** terrenos){//Lucas
 }
 
 void mostrarTerreno(Terrenos ** terrenos, int id){//Sarah Cristina
+    int i;
+    int encontrado = 0; // Variável para verificar se o terreno foi localizado
 
+    // Percorre o vetor de 100 posições
+    for (i = 0; i < 100; i++) {
+        if (terrenos[i] != NULL && terrenos[i]->id == id) {     //se o terreno for encontrado, exibe as seguintes informacoes:
+
+            encontrado = 1;
+            printf("--- INFORMAÇÕES DO TERRENO (ID %d) ---\n", terrenos[i]->id);
+            printf("Nome do Proprietário: %s\n", terrenos[i]->dono.nome);
+            printf("CPF do Proprietário: %s\n", terrenos[i]->dono.cpf);
+            printf("Data de Nascimento: %02d/%02d/%d\n", terrenos[i]->dono.data_nasc.dia, terrenos[i]->dono.data_nasc.mes, terrenos[i]->dono.data_nasc.ano);
+            printf("Comprimento do Terreno (m): %.2f\n", terrenos[i]->comprimento);
+            printf("Largura do Terreno (m): %.2f\n", terrenos[i]->largura);
+            printf("Área Total (m²): %.2f\n", terrenos[i]->area);
+            printf("Preço do Metro Quadrado (R$): %.2f\n", terrenos[i]->preco_m2);
+            printf("Data da Compra: %02d/%02d/%d\n", terrenos[i]->data_compra.dia, terrenos[i]->data_compra.mes, terrenos[i]->data_compra.ano);
+            break;
+        }
+    }
+    if (!encontrado) {      //se o terreno não foi encontrado, exibe o seguinte erro:
+        printf("\nERRO: Terreno com ID %d não encontrado ou posição vazia.\n", id);
+    }
 }
 
 void editarTerreno(Terrenos ** terrenos, int id){//Sarah Cristina
+    int i;
+    int encontrado = 0;
 
+    for (i = 0; i < 100; i++) {     //verifica se a posição do terreno e sse o ID sao validos
+        if (terrenos[i] != NULL && terrenos[i]->id == id) {     //se for encontrado, exibe a seguinte mensagem:
+            encontrado = 1;
+
+            printf("--- EDIÇÃO DO TERRENO (ID %d) ---\n", id);
+            printf("Insira os novos dados para o terreno com ID %d.\n", id);
+            
+            //aqui coletamos os novos dados do proprietário:
+            printf("Novo Nome do Proprietário: ");
+            scanf(" %[^\n]%*c", terrenos[i]->dono.nome);
+
+            printf("Novo CPF do Proprietário: ");
+            scanf(" %[^\n]%*c", terrenos[i]->dono.cpf);
+            
+            printf("Novo Telefone do Proprietário: ");
+            scanf(" %[^\n]%*c", terrenos[i]->dono.telefone); 
+            printf("Nova Data de Nascimento do Proprietário (dd/mm/aaaa): ");
+            scanf("%d/%d/%d", &terrenos[i]->dono.data_nasc.dia, &terrenos[i]->dono.data_nasc.mes, &terrenos[i]->dono.data_nasc.ano);
+            
+            //aqui coletamos os novos dados do terreno
+            printf("Novo Comprimento do Terreno (metros): ");
+            scanf("%f", &terrenos[i]->comprimento);
+            printf("Nova Largura do Terreno (metros): ");
+            scanf("%f", &terrenos[i]->largura);
+
+            //recalculamos a area (a area do terreno é dada pelo comprimento * largura):
+            terrenos[i]->area = terrenos[i]->comprimento * terrenos[i]->largura;
+            
+            printf("Área Total Recalculada (m²): %.2f\n", terrenos[i]->area); 
+            printf("Novo Preço do Metro Quadrado (R$): ");
+            scanf("%f", &terrenos[i]->preco_m2);
+
+            //nova data da compra:
+            printf("Nova Data da Compra (dd/mm/aaaa): ");
+            scanf("%d/%d/%d", &terrenos[i]->data_compra.dia, &terrenos[i]->data_compra.mes, &terrenos[i]->data_compra.ano);
+            
+            //limpa o buffer caso ele exista
+            while (getchar() != '\n'); 
+            
+            printf("\nO terreno com ID %d foi editado com sucesso!\n", id);
+            
+            break;
+        }
+    }
+    if (!encontrado) {      //se o terreno nao foi encontrado, exibe o seguinte erro:
+        printf("\nERRO: Terreno com ID %d não encontrado ou posição vazia.\n", id);
+    }
 }
 
 double calcularValorTerreno(Terrenos ** terrenos, int id){//Sara Raquel
